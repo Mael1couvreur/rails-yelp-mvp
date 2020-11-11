7bce76f4f25d8fab2922a380cf36ffea5e0b3ac2
Mael1couvreur <mael.couvreur@kedgebs.com>
2020-11-11 20:17:17 +0100

    Need styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'italian' }
+pizza_east = { name: 'Smart Green Corner', address: "20 rue Castelnau d'Auros 33800 Bordeaux France", category: 'chinese' }
+munchies = { name: 'Munchies', address: '21 rue des Augustins, Bordeaux', category: 'italian' }
+restO =  { name: "Rest'O", address: '16 quai de Bacalan, Bordeaux', category: 'japanese' }
+banana = { name: 'Banana Café', address: '5 cours Pasteur, Bordeaux', category: 'french' }
+wild = { name: 'Wild Note Vegan Burger', address: '20 rue Sanche de Pomiers 33000 Bordeaux', category: 'belgian' }
+
+[dishoom, pizza_east, munchies, restO, banana, wild].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
